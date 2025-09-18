e32100ffc6be890e0fea92f9d9a1432a52f3cab9
unknown <luis.dev.master@gmail.com>
2025-09-17 20:56:30 -0600

    creación de main.py y manual de usuario

---
+++ main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from VistaEmulador import VistaEmulador
+
+# ---------- main ----------
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = VistaEmulador(root)
+    root.mainloop()
